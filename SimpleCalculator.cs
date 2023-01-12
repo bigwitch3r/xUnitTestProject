8b5f4b6e2c61fe9c76ff6aa33e829632f75d296f
Данил <bigwitch3r@gmail.com>
2023-01-13 00:48:31 +0300

    Добавьте файлы проекта.

---
+++ SimpleCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestProject
+{
+    public class SimpleCalculator
+    {
+        public SimpleCalculator() { }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ SimpleCalculatorTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTestProject
+{
+    public class SimpleCalculatorTests
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange
+            var calculator = new SimpleCalculator();
+
+            // Act
+            var result = calculator.Add(2, 2);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void SubTest()
+        {
+            // Arrange
+            var calculator = new SimpleCalculator();
+
+            // Act
+            var result = calculator.Sub(2, 2);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MultTest() 
+        {
+            // Arrange
+            var calculator = new SimpleCalculator();
+
+            // Act
+            var result = calculator.Mult(2, 2);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void DivTest()
+        {
+            // Arrange
+            var calculator = new SimpleCalculator();
+
+            // Act
+            var result = calculator.Div(2, 2);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
